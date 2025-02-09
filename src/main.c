ae77ea06986654c844a1ccdd366d1135f757b4c5
JLJZ <52993042+JLJZ@users.noreply.github.com>
2025-02-09 19:09:46 +0800

    Add process launcher

--- src/main.c
+++ src/main.c
@@ -2,20 +2,31 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "shell.h"
 
+static void exec(const cmd_info *cmd) {
+    char *argv[cmd->argc + 1];
+    memcpy(argv, cmd->argv, sizeof(char *));
+    argv[cmd->argc] = NULL;
+
+    fprintf(stderr, "Error %d\n", execvp(cmd_name(cmd), argv));
+}
+
 int main(void) {
     bool should_exit = false;
+
     while (!should_exit) {
         cmd_info cmd = cmd_input("> ");
-        should_exit = strcasecmp("exit", cmd_name(&cmd));
+        should_exit = !strcasecmp("exit", cmd_name(&cmd));
 
-        for (int i = 0; i < cmd.argc; i++) {
-            puts(cmd.argv[i]);
+        if (!should_exit) {
+            exec(&cmd);
         }
 
         cmd_free(&cmd);
-    };
+    }
+
     return 0;
 }
