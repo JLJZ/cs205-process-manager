e142fc34ffd792615d55c0a56e209b15ed58c699
JLJZ <52993042+JLJZ@users.noreply.github.com>
2025-02-14 02:29:33 +0800

    Rename procman parameters from sp to pm

--- src/procman.c
+++ src/procman.c
@@ -248,15 +248,15 @@ static void dispatch(procman *pm, args *a) {
     }
 }
 
-static void pm_server_init(procman *sp) {
-    if (close(sp->pipe[1]) < 0) {
+static void pm_server_init(procman *pm) {
+    if (close(pm->pipe[1]) < 0) {
         error("close() pipe failed on server init");
         return;
     }
     
     while (true) {
         char *cmd_str = NULL;
-        size_t size = read_pipe(sp->pipe[0], &cmd_str);
+        size_t size = read_pipe(pm->pipe[0], &cmd_str);
 
         /* Ignore further processing on errors */
         if (size < 1) {
@@ -266,12 +266,12 @@ static void pm_server_init(procman *sp) {
         args a;
         args_cast(&a, cmd_str, size);
 
-        dispatch(sp, &a);
+        dispatch(pm, &a);
         
         args_free(&a);
     }
     
-    if (close(sp->pipe[0]) < 0) {
+    if (close(pm->pipe[0]) < 0) {
         error("close() pipe failed on server init");
         return;
     }
@@ -279,18 +279,18 @@ static void pm_server_init(procman *sp) {
     exit(EXIT_SUCCESS);
 }
 
-static void pm_client_init(procman *sp) {
-    if (close(sp->pipe[0]) < 0) {
+static void pm_client_init(procman *pm) {
+    if (close(pm->pipe[0]) < 0) {
         error("close() pipe failed on client init");
         return;
     }
 }
 
 procman *pm_init(void) {
-    procman *sp = calloc(1, sizeof(procman));
-    sp->processes = NULL;
+    procman *pm = calloc(1, sizeof(procman));
+    pm->processes = NULL;
     
-    if (pipe(sp->pipe) < 0) {
+    if (pipe(pm->pipe) < 0) {
         error("pipe() init failed");
         goto err;
     }
@@ -303,53 +303,53 @@ procman *pm_init(void) {
         goto err;
 
     case 0:
-        sp->server_pid = getpid();
-        pm_server_init(sp);
+        pm->server_pid = getpid();
+        pm_server_init(pm);
         break;
 
     default:
-        sp->server_pid = pid;
-        pm_client_init(sp);
+        pm->server_pid = pid;
+        pm_client_init(pm);
         break;
     }
     
-    return sp;
+    return pm;
 err:
-    free(sp);
+    free(pm);
     return NULL;
 }
 
-void pm_execute(procman *sp, const char *cmd_string) {
+void pm_execute(procman *pm, const char *cmd_string) {
     args cmd;
     args_parse(&cmd, cmd_string);
     
-    if (write(sp->pipe[1], cmd.bytes, cmd.bytes_size) < 0) {
+    if (write(pm->pipe[1], cmd.bytes, cmd.bytes_size) < 0) {
         error("write() pipe failed on client");
     }
     
     args_free(&cmd);
 }
 
-void pm_shutdown(procman *sp, unsigned int timeout) {
+void pm_shutdown(procman *pm, unsigned int timeout) {
     int status;
     pid_t pid = 0;
-    pid = waitpid(sp->server_pid, &status, WNOHANG);
+    pid = waitpid(pm->server_pid, &status, WNOHANG);
     
     do {
-        if (kill(sp->server_pid, SIGTERM) < 0) {
-            if (kill(sp->server_pid, SIGKILL) < 0) {
+        if (kill(pm->server_pid, SIGTERM) < 0) {
+            if (kill(pm->server_pid, SIGKILL) < 0) {
                 error("error sending kill signals to server");
             }
         }
 
-        pid = waitpid(sp->server_pid, &status, WNOHANG);
+        pid = waitpid(pm->server_pid, &status, WNOHANG);
         sleep(1);
 
-    } while (pid != sp->server_pid && timeout-- > 0);
+    } while (pid != pm->server_pid && timeout-- > 0);
     
     if (pid == -1) {
         error("failed to properly shutdown server")
     }
 
-    free(sp);
+    free(pm);
 }
