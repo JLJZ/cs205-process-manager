2a23a3ff8ddba77b042e329905635d958ce302af
JLJZ <52993042+JLJZ@users.noreply.github.com>
2025-02-22 23:45:55 +0800

    Update read_all() docs

--- src/input.h
+++ src/input.h
@@ -3,7 +3,12 @@
 
 /**
  * @brief Read until EOF
- * 
+ *
+ * @param fd Input file descriptor
+ * @param buffer_size The number of bytes to read at once.
+ * 64 bytes if 0 specified
+ * @param terminator Stop reading when terminator is reached. Not included in
+ * the output string
  * @return char* Read string
  */
 char *read_all(int fd, size_t buffer_size, char terminator);
